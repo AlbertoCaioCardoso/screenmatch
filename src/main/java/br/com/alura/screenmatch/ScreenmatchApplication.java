4382098e4b6a00789775cc967b206d24995f77bb
Alberto Cardoso <albertocaiobr@hotmail.com>
2026-02-03 14:55:39 -0300

    Sobrescrevendo método apenas para visualizar comportamento de inicialização do Spring

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,15 +1,21 @@
 package br.com.alura.screenmatch;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScreenmatchApplication {
+public class ScreenmatchApplication implements CommandLineRunner {
 
     //https://www.omdbapi.com/?t=top+gun&apikey=9a3513ec&
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenmatchApplication.class, args);
 	}
+
+    @Override
+    public void run(String... args)throws Exception {
+        System.out.println("Primeiro projeto Spring sem Web");
+    }
 
 }
