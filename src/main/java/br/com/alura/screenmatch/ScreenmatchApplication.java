48cbd040230d77fac1c8ec7a693bdea82317958a
Alberto Cardoso <albertocaiobr@hotmail.com>
2026-02-03 14:36:45 -0300

    first commit

---
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,15 @@
+package br.com.alura.screenmatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication {
+
+    //https://www.omdbapi.com/?t=top+gun&apikey=9a3513ec&
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+}
