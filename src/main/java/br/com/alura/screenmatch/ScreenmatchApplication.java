0724c7ad17d2889b79120c4aa45505762a66691d
Alberto Cardoso <albertocaiobr@hotmail.com>
2026-03-11 10:03:49 -0300

    Enviando dados atualizados

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,5 +1,9 @@
 package br.com.alura.screenmatch;
 
+import br.com.alura.screenmatch.model.DadosEpisodio;
+import br.com.alura.screenmatch.model.DadosSerie;
+import br.com.alura.screenmatch.services.ConsumoApi;
+import br.com.alura.screenmatch.services.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,8 +18,17 @@ public class ScreenmatchApplication implements CommandLineRunner {
 	}
 
     @Override
-    public void run(String... args)throws Exception {
-        System.out.println("Primeiro projeto Spring sem Web");
+    public void run(String... args) throws Exception {
+        var consumoApi = new ConsumoApi();
+        var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=9a3513ec");
+        //System.out.println(json);
+        ConverteDados conversor = new ConverteDados();
+        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+        System.out.println(dados);
+        json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=9a3513ec");
+        DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
+        System.out.println(dadosEpisodio);
+
     }
 
 }
