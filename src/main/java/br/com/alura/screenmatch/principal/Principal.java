e1d497e4e68f6b4ee0fd690b3084464bf36e35f8
Alberto Cardoso <albertocaiobr@hotmail.com>
2026-03-18 10:58:17 -0300

    Testando Streams e listas

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -6,9 +6,8 @@ import br.com.alura.screenmatch.model.DadosTemporada;
 import br.com.alura.screenmatch.services.ConsumoApi;
 import br.com.alura.screenmatch.services.ConverteDados;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Principal {
 
@@ -32,7 +31,7 @@ public class Principal {
             DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
             temporadas.add(dadosTemporada);
         }
-        //temporadas.forEach(System.out::println);
+        temporadas.forEach(System.out::println);
 
 //        for(int i = 0; i < dados.totalTemporadas(); i++){
 //            List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
@@ -42,5 +41,18 @@ public class Principal {
 //        }
 
         temporadas.forEach(t->t.episodios().forEach(e-> System.out.println(e.titulo())));
+
+        //iterando com streams sob as temporadas, acessando sua lista de episodios e criando um stream com esses episodios para gerar nova lista
+        List<DadosEpisodio> dadosEpisodios =
+                temporadas.stream()
+                .flatMap(t -> t.episodios().stream())
+                        .collect(Collectors.toList());
+
+        System.out.println("\nTop 5 episódios: ");
+        dadosEpisodios.stream()
+                .filter(f -> !f.avaliacao().equalsIgnoreCase("N/A"))
+                .sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
+                .limit(5)
+                .forEach(System.out::println);
     }
 }
